5ef263e22840e92a6bee77ec90fd8ecf09d889f9
kimyuri <dsuis1310009@naver.com>
2023-02-23 19:29:01 +0900

    1:n

---
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -0,0 +1,26 @@
+package org.zerock.board.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    //한개의 row(object) 내에 Object[]로 나옴
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+    @Query("select b, r from Board b left join Reply r on r.board = b where b.bno = :bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value = "SELECT b, w, count(r) FROM Board b LEFT JOIN b.writer w LEFT JOIN Reply r ON r.board = b GROUP BY b"
+            , countQuery = "SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable); //목록화면에 필요한 데이터
+
+    @Query("select b, w, count(r) from Board b left join b.writer w left outer join Reply r on r.board = b where b.bno = :bno")
+    Object getBoardByBno(@Param("bno") Long bno);
+}
