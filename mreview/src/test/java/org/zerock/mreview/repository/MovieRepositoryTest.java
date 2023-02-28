c8d6185b21a5d20b3192f1e6bdb592fac4b3a580
kimyuri <dsuis1310009@naver.com>
2023-02-28 14:01:27 +0900

    review

--- mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTest.java
+++ mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTest.java
@@ -3,11 +3,16 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -20,6 +25,7 @@ class MovieRepositoryTest {
     @Autowired
     private MovieImageRepository imageRepository;
 
+    // 영화 생성
     @Commit
     @Transactional
     @Test
@@ -43,5 +49,17 @@ class MovieRepositoryTest {
             }
             System.out.println("=========================");
         });
+    }
+
+    //영화 목록 조회
+    @Test
+    void testListPage() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "mno"));
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
+
+        for (Object[] objects : result.getContent()) {
+            System.out.println(Arrays.toString(objects));
+        }
+
     }
 }
