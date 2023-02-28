3a899dcd53f3bbc68dcada18f1f5213a9039de3c
kimyuri <dsuis1310009@naver.com>
2023-02-28 15:30:24 +0900

    jpql error

--- mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long>{
-    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
-            + "left join MovieImage mi on mi.movie = m "
-            + "left join Review r on r.movie = m group by m")
+    @Query("select m, mi, avg(coalesce(r.grade,0.0)),  count(distinct r.reviewnum) " +
+            "from Movie m " +
+            "left outer join MovieImage mi on mi.movie = m "+
+            "left outer join Review r on r.movie = m " +
+            "group by m")
     Page<Object[]> getListPage(Pageable pageable);
 }
 
